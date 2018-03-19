2e1cd53a89c564c7f0a0167544164b8a10ebbb31
jason <jsn.ngyn@gmail.com>
2018-03-19 14:11:44 -0400

    optimized prime function

--- problem_03.py
+++ problem_03.py
@@ -5,10 +5,13 @@ import math
 n = 600851475143
 
 def is_prime(n):
-    for number in range(2, n):
+    if n == 1:
+        return False
+    for number in range(2, math.ceil(n**0.5)):
         if n % number == 0:
             return False
     return True
 
 prime_factors = [number for number in range(1, math.ceil(n**0.5)) if n % number == 0 and is_prime(number)]
-print(max(prime_factors))
+
+print(prime_factors)
